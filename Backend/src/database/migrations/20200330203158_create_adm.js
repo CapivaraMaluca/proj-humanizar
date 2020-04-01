e23ef1318dcc46a2a164ebccf02d970833af50fe
wjulio <wolgranjulio@gmail.com>
2020-03-31 21:05:34 -0300

    Criação da tabela adm - simples códigos em routes e index js

--- Backend/src/database/migrations/20200330203158_create_adm.js
+++ Backend/src/database/migrations/20200330203158_create_adm.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('adm',function(table){
+        table.increments();//chave primaria autoincrement
+        table.string('nome').notNullable();
+        table.string('senha').notNullable();
+    });
+    
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('adm');
 };
