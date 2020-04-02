008db0952506e94d3dead8f6f84b77c95f20efc0
wjulio <wolgranjulio@gmail.com>
2020-04-02 16:04:07 -0300

    Corrigindo: Tabela especialidade

--- Backend/src/database/migrations/20200402151222_create_especialidade.js
+++ Backend/src/database/migrations/20200402151222_create_especialidade.js
@@ -4,12 +4,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('especialidade', function (table) {
         table.integer('id_especialidade').primary();
         table.varchar('especialidade').notNullable();
-        table.time('hora_Disponibilidade_inicia').notNullable();
-        table.time('hora_Disponibilidade_final').notNullable();
-        table.varchar('dias_disponibilidade').notNullable();
-        
+        //
         table.integer('voluntario_cpf_voluntario').notNullable();
-        //chave estrangeira
         table.foreign('voluntario_cpf_voluntario').references('cpf').inTable('voluntario');
         })
 };
