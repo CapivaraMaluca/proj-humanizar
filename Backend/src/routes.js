1642f9af40a3e40f2fbffeffe65e40c2b1e3115c
wjulio <wolgranjulio@gmail.com>
2020-04-06 15:32:40 -0300

    Criando pasta e arquivos Controllers e editando Routes,js

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const routes = express.Router()
+const connection = require('./database/conection');
+
+const loginController = require('./controllers/loginController');
+const cadastroController = require('./controllers/cadastroController');
+
+
 
 
 
